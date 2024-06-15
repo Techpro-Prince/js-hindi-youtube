5631b8ff55475bae5048f3f608da55ccc38e5901
Prince Singh <108799440+Techpro-Prince@users.noreply.github.com>
2024-06-15 14:27:54 +0000

    comm,it my changes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,16 @@
+const accountId=4;
+let accountEmail="psingh@uhm.com";
+var accountPassword="123";
+accountCity="Jaipur"
+/*
+prefer not use var 
+because issue is to block scope functional scope_variables
+*/
+accountEmail="test@uhm.com"
+accountPassword="123123"
+accountCity="Gorakhpur"
+
+// console.table([accountEmail,accountPassword,accountCity])
+console.log(typeof undefined)
+console.log(typeof Object)
+console.log(typeof null)
